f3f05314bbe937125a61f4bbce0d9da196472ff2
Ismael Ruiz Ranz <35577277+Equipo45@users.noreply.github.com>
2023-10-17 18:27:00 +0200

    Time: 22 ms (39.04%), Space: 8.9 MB (53.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,24 +5,30 @@ public:
 
         std::string stringTotal = "";
         std::map<char, int> charIndex;
-        int maxCharacter = 0;
+        int maxCharacterLength = 0;
         int start = 0;
-
+        //Empezamos en el indice 1 del string
         for (int i = 0; i < s.length(); i ++) {
             char caracter = s[i];
-
+            //Buscamos si el carcater esta dentro del map ya
+            //Y si no es un caracter guardado previamente
             if (charIndex.find(caracter) != charIndex.end() && charIndex[caracter] >= start) {
+                //Ponemos el inicio en el siguiente indice al caracter encontrado
                 start = charIndex[caracter] + 1;
             }
             
+            //Guardamos el indice del caracter actual
             charIndex[caracter] = i;
-            int currentChar = charIndex[caracter] - start + 1;
-            if (maxCharacter < currentChar) {
-                maxCharacter = currentChar;
+            //Calcumos la distancia del caracter maximo actual
+            //Sera el indice del carcater actual - el indice donde se empezo a buscar el caracter, caracteres previos a donde se empeiza no se tendran en cuenta, se le suma 1 por que se empieza contantdo desde el 0
+            int currentCharLength = charIndex[caracter] - start + 1;
+            //Compara el caracter mas grande con el actual
+            if (maxCharacterLength < currentCharLength) {
+                maxCharacterLength = currentCharLength;
             }
 
         }
 
-        return maxCharacter;
+        return maxCharacterLength;
     }
 };
