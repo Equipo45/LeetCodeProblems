5a061bb78e897aced6e73aa8932cb600c8ef2319
Ismael Ruiz Ranz <35577277+Equipo45@users.noreply.github.com>
2023-10-19 18:30:21 +0200

    Time: 0 ms (100%), Space: 10.1 MB (7.97%) - LeetHub

--- 1242-matrix-block-sum/1242-matrix-block-sum.cpp
+++ 1242-matrix-block-sum/1242-matrix-block-sum.cpp
@@ -3,28 +3,35 @@ public:
     //This is a tow moving window , one in columns and another in rows
     //
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
+    int m = mat.size();
+    int n = mat[0].size();
 
-        vector<vector<int>> matAnswer(mat.size(), vector<int>(mat[0].size(),0));
-
-        for (int i = 0 ; i < mat.size() ; i++) {
-            int rowInit = ((i - k) <= 0) ? 0 : i - k;
-            int rowEnd = ((i + k) >= mat.size()) ? mat.size() - 1 : i + k;
-            for (int j = 0 ; j < mat[i].size() ; j++) {
-                int colInit = ((j - k) <= 0) ? 0 : j - k;
-                int colEnd = ((j + k) >= mat[0].size()) ? mat[0].size() - 1 : j + k;              
-                matAnswer[i][j] = matrixSum(mat, rowInit, rowEnd, colInit,colEnd);
-            }
+    // Calculate the cumulative sum for the original matrix mat
+    vector<vector<int>> cumSum(m, vector<int>(n, 0));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cumSum[i][j] = mat[i][j];
+            if (i > 0) cumSum[i][j] += cumSum[i - 1][j];
+            if (j > 0) cumSum[i][j] += cumSum[i][j - 1];
+            if (i > 0 && j > 0) cumSum[i][j] -= cumSum[i - 1][j - 1];
         }
-        return matAnswer;
     }
 
-    int matrixSum (vector<vector<int>>& mat, int rowInit, int rowEnd, int colInit,int colEnd) {
-        int sum = 0;
-        for (int i = rowInit ; i <= rowEnd; i++) {
-            for (int j = colInit ; j <= colEnd; j++) {
-                sum += mat[i][j];
-            }
+    vector<vector<int>> matAnswer(m, vector<int>(n, 0));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            int rowInit = max(0, i - k);
+            int rowEnd = min(m - 1, i + k);
+            int colInit = max(0, j - k);
+            int colEnd = min(n - 1, j + k);
+
+            matAnswer[i][j] = cumSum[rowEnd][colEnd];
+            if (rowInit > 0) matAnswer[i][j] -= cumSum[rowInit - 1][colEnd];
+            if (colInit > 0) matAnswer[i][j] -= cumSum[rowEnd][colInit - 1];
+            if (rowInit > 0 && colInit > 0) matAnswer[i][j] += cumSum[rowInit - 1][colInit - 1];
         }
-        return sum;
     }
+
+    return matAnswer;
+}
 };
