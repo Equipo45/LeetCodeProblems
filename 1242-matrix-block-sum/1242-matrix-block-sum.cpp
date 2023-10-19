f3601bd740e125888c5648d06ed677fa4ec8f84a
Ismael Ruiz Ranz <35577277+Equipo45@users.noreply.github.com>
2023-10-19 18:48:14 +0200

    Time: 601 ms (20.94%), Space: 9.8 MB (34.84%) - LeetHub

--- 1242-matrix-block-sum/1242-matrix-block-sum.cpp
+++ 1242-matrix-block-sum/1242-matrix-block-sum.cpp
@@ -7,11 +7,11 @@ public:
         vector<vector<int>> matAnswer(mat.size(), vector<int>(mat[0].size(),0));
 
         for (int i = 0 ; i < mat.size() ; i++) {
-            int rowInit = (i - k) <= 0 ? 0 : i - k;
-            int rowEnd = (i + k) >= mat.size() ? mat.size() - 1 : i + k;
+            int rowInit = max(0 ,i - k);
+            int rowEnd = min(int(mat.size() - 1),i + k);
             for (int j = 0 ; j < mat[i].size() ; j++) {
-                int colInit = (j - k) <= 0 ? 0 : j - k;
-                int colEnd = (j + k) >= mat[0].size() ? mat[0].size() - 1 : j + k;              
+                int colInit = max(0 ,j - k);
+                int colEnd = min(int(mat[0].size() - 1),j + k);              
                 matAnswer[i][j] = matrixSum(mat, rowInit, rowEnd, colInit,colEnd);
             }
         }
